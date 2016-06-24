35b789e648c2a2f00563c230db07179d04e45226
Shivya B <sbalac17@my.centennialcollege.ca>
2016-06-24 12:10:06 -0400

    MidTerm Test

---
+++ COMP123-MidTermExam/LottoGame.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_MidTermExam
+{
+    /**
+     * <summary>
+     * This abstract class is a blueprint for all Lotto Games
+     * </summary>
+     * 
+     * @class LottoGame
+     * @property {int} ElementNum;
+     * @property {int} SetSize;
+     */
+    public abstract class LottoGame
+    {
+
+
+        // PRIVATE INSTANCE VARIABLES +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        private List<int> _elementList;
+         private int _elementNumber;
+        private List <int> _numberList;
+       Random _random = new Random();
+        private int _setSize;
+       
+
+        // CREATE private fields here --------------------------------------------
+
+        // PUBLIC PROPERTIES ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        // CREATE public properties here -----------------------------------------
+
+        // CONSTRUCTORS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        /**
+         * <summary>
+         * This constructor takes two parameters: elementNumber and setSize
+         * The elementNumber parameter has a default value of 6
+         * The setSize parameter has a default value of 49
+         * </summary>
+         * 
+         * @constructor LottoGame
+         * @param {int} elementNumber
+         * @param {int} setSize
+         */
+        public LottoGame(int elementNumber = 6, int setSize = 49)
+        {
+            // assign elementNumber local variable to the ElementNumber property
+            this.ElementNumber = elementNumber;
+
+            // assign setSize local variable tot he SetSize property
+            this.SetSize = setSize;
+
+            // call the _initialize method
+            this._initialize();
+
+            // call the _build method
+            this._build();
+        }
+
+        public List<int> ElementList
+      
+         
+        {
+            get { return this._elementList; }
+            set { this._elementList = value; }
+        }
+
+        public int ElementNumber
+
+        {
+            get { return this._elementNumber; }
+            set { this._elementNumber = value; }
+        }
+        public List<int> NumberList
+
+
+        {
+            get { return this._numberList; }
+          
+        }
+        public  Random
+            {
+
+            get { return this._random; }
+
+    }
+        public int SetSize
+
+
+        {
+            get { return this._setSize; }
+            set { this._setSize = value; }
+        }
+
+        // PRIVATE METHODS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        // CREATE the private _initialize method here -----------------------------
+        private void _initialize()
+        {
+            this._elementList = new List<int>();
+            this._numberList = new List<int>();
+            this._random = new Random();
+
+
+        }
+
+
+
+
+
+
+        // CREATE the private _build method here -----------------------------------
+        private void _build()
+        {
+    for (int i = 0; i < setSize; i++)
+    {
+
+    }
+
+        }
+
+        // OVERRIDEN METHODS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        /**
+         * <summary>
+         * Override the default ToString function so that it displays the current
+         * ElementList
+         * </summary>
+         * 
+         * @override
+         * @method ToString
+         * @returns {string}
+         */
+        public override string ToString()
+        {
+            // create a string variable named lottoNumberString and intialize it with the empty string
+            string lottoNumberString = String.Empty;
+
+            // for each lottoNumber in ElementList, loop...
+            foreach (int lottoNumber in ElementList)
+            {
+                // add lottoNumber and appropriate spaces to the lottoNumberString variable
+                lottoNumberString += lottoNumber > 9 ? (lottoNumber + " ") : (lottoNumber + "  ");
+            }
+
+            return lottoNumberString;
+        }
+
+// PUBLIC METHODS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// CREATE the public PickElements method here ----------------------------
+public void PickElement()
+{
+    if (ElementList > 0 )
+    { 
+
+    for (int i = 0; i < SetSize; i++)
+        list.Add(i);
+    return list.To();
+}
+   }
+else
+return false; 
